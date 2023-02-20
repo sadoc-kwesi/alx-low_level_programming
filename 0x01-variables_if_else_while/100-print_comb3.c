d235cd7180d58e39ae3767e2dd3f68e260ff7fb1
sadoc-kwesi <bmyarquah@gmail.com>
2023-02-20 14:08:35 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int s = 0, e = 8, s1 = 1, e2 = 9, tmp = 1;
+
+	while (s <= e)
+	{
+		s1 = tmp;
+
+		while (s1 <= e2)
+		{
+			putchar(s + '0');
+			putchar(s1 + '0');
+			s1++;
+
+			if (s != e)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+		tmp++;
+		s++;
+	}
+
+	putchar('\n');
+	return (0);
+}
