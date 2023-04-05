28c601c7468aefaab98f20d42fe062732db43ac9
sadoc-kwesi <bmyarquah@gmail.com>
2023-04-04 18:34:48 -0700

    Update 0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/1-main.c
+++ 0x13-more_singly_linked_lists/1-main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+
 /**
 * main - check the code
 *
@@ -24,7 +25,7 @@ return (1);
 new->n = 9;
 new->next = head;
 head = new;
-n = print_listint(head);
+n = listint_len(head);
 printf("-> %lu elements\n", n);
 free(new);
 return (0);
